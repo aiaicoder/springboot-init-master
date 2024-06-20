651c78a53c7bd88ebc03756a70110a8d3e570a20
aiaicoder <1124655410@qq.com>
2024-06-20 21:23:05 +0800

    添加了一个sql插件可以优雅的实现sql的打印，记录sql执行时间

--- src/main/java/com/xin/springbootinit/config/MyBatisPlusConfig.java
+++ src/main/java/com/xin/springbootinit/config/MyBatisPlusConfig.java
@@ -1,7 +1,9 @@
 package com.xin.springbootinit.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +29,12 @@ public class MyBatisPlusConfig {
         // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer mybatisConfigurationCustomizer() {
+        return configuration -> {
+            configuration.addInterceptor(new SqlInterceptor());
+        };
     }
 }
